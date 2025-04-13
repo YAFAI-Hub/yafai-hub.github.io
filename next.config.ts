aaa50583a250cd1e9068f007fbb633caa1e8ccaa
ssbodapati <susanth.bodapati@gmail.com>
2025-04-13 15:09:59 +0530

    patch

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+
+// Determine base path based on environment variable set by GitHub Actions
+const basePath = process.env.PAGES_BASE_PATH || '';
 
 const nextConfig: NextConfig = {
   /* config options*/
   output: 'export',
+  basePath: basePath,
+  assetPrefix: basePath ? `${basePath}/` : '/',
   images: {
     remotePatterns: [
       {
