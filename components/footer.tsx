7852050a0511cd0bb6c03ebaac3f747d2eef5285
Tarun <tarun.akkreddygari@betaflux.co>
2025-04-13 13:03:33 +0530

    init release

---
+++ components/footer.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { BotMessageSquareIcon, HeartIcon, TriangleIcon } from "lucide-react";
+
+export function Footer() {
+  return (
+    <footer className="border-t w-full h-16">
+      <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
+        <div className="flex items-center justify-center w-full gap-2">
+          <BotMessageSquareIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />
+          <span>YAFAI</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export function FooterButtons() {
+  return (
+    <>
+      <Link
+        href="https://vercel.com/templates/next.js/documentation-template"
+        className={buttonVariants({ variant: "outline", size: "sm" })}
+      >
+        <TriangleIcon className="h-[0.8rem] w-4 mr-2 text-primary fill-current" />
+        Deploy
+      </Link>
+      <Link
+        href="https://github.com/sponsors/nisabmohd"
+        className={buttonVariants({ variant: "outline", size: "sm" })}
+      >
+        <HeartIcon className="h-4 w-4 mr-2 text-red-600 fill-current" />
+        Sponsor
+      </Link>
+    </>
+  );
+}
