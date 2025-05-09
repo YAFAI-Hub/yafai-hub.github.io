97cf4849d4e2b8514e96d1996eb66f5ba4a63b02
ssbodapati <susanth.bodapati@gmail.com>
2025-05-09 21:12:51 +0530

    patch

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex sm:min-h-[87.5vh] min-h-[82vh] flex-col sm:items-center justify-center text-center sm:py-8 py-14">
       <Link
-        href="https://github.com/YAFAI-Hub/core"
+        href="https://github.com/YAFAI-Hub"
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4 sm:-mt-12"
       >
@@ -15,11 +15,10 @@ export default function Home() {
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
       <h1 className="text-[1.80rem] leading-8 sm:px-8 md:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center">
-        Define and Deploy Agentic interfaces.
+      Turn backend APIs into Conversations
       </h1>
       <p className="mb-8 md:text-lg text-base  max-w-[1200px] text-muted-foreground text-left sm:text-center">
-        Consume YAFAI multi agent system on any platform mobile,web,desktop or
-        even terminal via YAFAI Link
+      Define and deploy multi agent systems on mobile,web,desktop or even terminal.
       </p>
       <div className="sm:flex sm:flex-row grid grid-cols-2 items-center sm;gap-5 gap-3 mb-8">
         <Link
